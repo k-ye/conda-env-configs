052634cbca0981d9f343f26bdde2a117a7143001
Ye Kuang <yekuang.ky@gmail.com>
2020-02-26 17:56:31 +0900

    update taichi-dev conda env

--- taichi-dev/scripts/activate_env_vars.sh
+++ taichi-dev/scripts/activate_env_vars.sh
@@ -9,5 +9,6 @@ set -x
 export TAICHI_REPO_DIR=$HOME/Github/taichi
 export PYTHONPATH=$TAICHI_REPO_DIR/python/:$PYTHONPATH
 export PATH=$TAICHI_REPO_DIR/bin/:$PATH
+export TI_TEST_THREADS=4
 
 set +x
