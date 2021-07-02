3df2bf493c555aa58578877135a59d9040bcf62a
Ye Kuang <yekuang.ky@gmail.com>
2021-07-02 14:56:06 +0800

    wabt

--- taichi-dev/scripts/activate_env_vars.sh
+++ taichi-dev/scripts/activate_env_vars.sh
@@ -5,13 +5,14 @@
 export SAVED_PYTHONPATH=$PYTHONPATH
 # This has been installed to the system-wide directory
 export LLVM10_ROOTDIR=/usr/local
+export WABT_BINDIR=$HOME/Github/wabt/build
 
 set -x
 
 export TAICHI_REPO_DIR=$HOME/Github/taichi
 export PYTHONPATH=$TAICHI_REPO_DIR/python:$PYTHONPATH
 export LLVM_DIR=$LLVM10_ROOTDIR/lib/cmake/llvm
-export PATH=$LLVM10_ROOTDIR/bin:$PATH
+export PATH=$LLVM10_ROOTDIR/bin:$WABT_BINDIR:$PATH
 export PATH=$TAICHI_REPO_DIR/bin:$PATH
 export TI_TEST_THREADS=4
 
